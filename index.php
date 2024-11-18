ba339b275d18cde87981749290f4effb36af967a
imamprayudi <imamprayudi@yahoo.com>
2024-11-18 13:42:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GIT</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+  <header>
+  
+</header>
+<div class="container-fluid">
+<h2>GIT SERVICES FOR PT JVCKENWOOD ELECTRONICS INDONESIA</h2>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="http://103.164.195.166/custom" target="_self" >JKEI IT INVENTORY</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/jeinwebmysql" target="_blank">JKEI EDI SYSTEM</a>
+  </li>
+</ul>
+   
+<article>
+    <h1>Tentang Kami</h1>
+    <p>PT. Graha Indomedia Telekomunika (GIT) adalah perusahaan yang berkecimpung dalam industri multimedia dan telekomunikasi sejak tahun 2005. GIT memiliki kapasitas dan pengalaman yang luar biasa selama masa kerjanya. Dengan memiliki klien dari berbagai jenis bidang usaha, membuat GIT menjadi ahli di bidangnya.</p>
+    <p>Sesuai dengan tuntutan pekerjaan GIT yang didukung tenaga ahli profesional yang berpengalaman memberikan layanan jasa solusi total mulai dari repairing, perancangan, hingga implementasi.</p>
+</article>
+
+<img src="https://git.co.id/wp-content/uploads/2021/02/logoGIT2021-1024x1024.png"
+style="width:250px;height:250px;" class="mx-auto d-block">
+
+<footer class="blockqoute-footer">
+<p class="text-center">Jl. Kembar 1 No 53 Bandung</p>
+<p class="text-center">Apartemen Sudirman Park Tower B Unit 210A</p>
+<p class="text-center">Jl. K.H Mas Mansyur Kav 35 Jakarta Pusat</p>
+<p class="text-center">Copyright © 2023 Graha Indomedia Telekomunika</p>
+</footer>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>  
+</body>
+</html>
+
+
+
