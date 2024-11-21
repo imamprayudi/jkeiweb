be9a959976d59a4b82cbc97d9f0f65a35adcff5e
imamprayudi <imamprayudi@gmail.com>
2024-11-21 15:37:33 +0700

    Start Index PHP

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="assets/css/jein.css" rel="stylesheet" type="text/css" />
+  <script src="assets/js/jquery.js"></script>
+  <title>JKEI WEB</title>
+</head>
+<body>
+<div id="outer">
+  <div class="kepala">
+    <img border="0" src="assets/images/jvclogo.png" alt="JVCKENWOOD" width="419" height="120" />
+  </div>
+  <div class="cssmenu">
+    <ul>
+      <li class='active '><a href='index.php'><span>Home</span></a></li>
+      <li><a href='contents/login.php'><span>Login</span></a></li>
+      <li><a href='https://scm.actron.com.my/modules/admin/login.aspx'><span>Actron Web</span></a></li>
+    </ul>
+  </div>
+</div>  <!-- div=outer -->
+
+<div class="badan">
+  <img border="0" src="assets/gambar/building.gif" alt="JVCKENWOOD" width="345" height="222" />
+</div>
+
+<div class="kaki">
+  <br /><br /><br /><br />
+  <h2>PT JVCKENWOOD ELECTRONICS INDONESIA<br /></h2>
+  <h3>JL.Surya Lestari Kav.I-16B Suryacipta City of Industry Desa Kutamekar Kecamatan Ciampel Kabupaten Karawang Jawa Barat 41363, INDONESIA.</h3>
+</div>
+</body>
+</html>
